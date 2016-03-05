f5e67c3e398a6e7c24068e846e4a365764b4bb04
shiningsilence <zhangwenlonggit@163.com>
2016-03-05 15:23:16 +0800

    first update  demo

---
+++ mydemo/src/main/java/com/sdemo/mydemo/MainActivity.java
@@ -0,0 +1,15 @@
+package com.sdemo.mydemo;
+
+import android.os.Bundle;
+
+/**
+ * Created by dell on 2016/3/5.
+ */
+public class MainActivity extends  ActionBarBaseActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        //setToolBarTitle("extends from baseactivity");
+    }
+}
